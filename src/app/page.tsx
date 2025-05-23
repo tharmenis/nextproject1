100ff0a8b9c049cdd3ca337e602d65113be0b11f
Thodoris Armenis <tharmenis@gmail.com>
2025-05-23 15:49:45 +0300

    added paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold">Hello Antoni</h1>
+      <p> A new paragraph with a story to tell</p>
     </main>
   );
 }
